2723b1d8f121ab4efc1ae383f1a480cc3ab8a264
Kevin Barrett <kevincb45@gmail.com>
2018-11-11 23:43:16 -0500

    Fixed a minor server side error

--- src/fileResponses.js
+++ src/fileResponses.js
@@ -3,18 +3,18 @@ const fs = require('fs');
 const index = fs.readFileSync(`${__dirname}/../client/client.html`);
 const styles = fs.readFileSync(`${__dirname}/../client/styles.css`);
 
-//js
+// js
 const main = fs.readFileSync(`${__dirname}/../client/js/main.js`);
 const classes = fs.readFileSync(`${__dirname}/../client/js/classes.js`);
 const loader = fs.readFileSync(`${__dirname}/../client/js/loader.js`);
 const interaction = fs.readFileSync(`${__dirname}/../client/js/interaction.js`);
 const utilities = fs.readFileSync(`${__dirname}/../client/js/utilities.js`);
 const stateManager = fs.readFileSync(`${__dirname}/../client/js/stateManager.js`);
-const player =  fs.readFileSync(`${__dirname}/../client/js/player.js`);
-const enemy =  fs.readFileSync(`${__dirname}/../client/js/enemy.js`);
-const grid =  fs.readFileSync(`${__dirname}/../client/js/grid.js`);
+const player = fs.readFileSync(`${__dirname}/../client/js/player.js`);
+const enemy = fs.readFileSync(`${__dirname}/../client/js/enemy.js`);
+const grid = fs.readFileSync(`${__dirname}/../client/js/grid.js`);
 
-//media
+// media
 const termite = fs.readFileSync(`${__dirname}/../client/media/termite.png`);
 
 
@@ -31,9 +31,9 @@ const getStyles = (request, response) => {
 };
 
 const getJS = (request, response, path) => {
-  response.writeHead(200, { 'Content-Type': 'application/javascript'});
+  response.writeHead(200, { 'Content-Type': 'application/javascript' });
 
-  switch(path){
+  switch (path) {
     case '/js/main.js':
       response.write(main);
       break;
@@ -61,26 +61,30 @@ const getJS = (request, response, path) => {
     case '/js/grid.js':
       response.write(grid);
       break;
-  }
-
-  response.end();
-}
-
-const getSprites = (request, response, path) => {
-  response.writeHead(200, { 'Content-Type': 'image/png'});
-
-  switch(path){
-    case '/media/termite.png':
-      response.write(termite);
+    default:
       break;
   }
 
   response.end();
-}
+};
+
+const getSprites = (request, response, path) => {
+  response.writeHead(200, { 'Content-Type': 'image/png' });
+
+  switch (path) {
+    case '/media/termite.png':
+      response.write(termite);
+      break;
+    default:
+      break;
+  }
+
+  response.end();
+};
 
 module.exports = {
   getIndex,
   getStyles,
   getJS,
-  getSprites
+  getSprites,
 };
